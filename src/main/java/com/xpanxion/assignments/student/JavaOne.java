2c89c51d1dab03c50dfc974f926e253900bea65a
Raj Rana <raj.rana@fiserv.com>
2023-10-23 11:30:35 -0400

    Completed ex4

--- src/main/java/com/xpanxion/assignments/student/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student/JavaOne.java
@@ -53,12 +53,12 @@ public class JavaOne {
         int n = input.length();
         for (int i = 0; i < (n/2); ++i) {
             if (input.charAt(i) != input.charAt(n - i - 1)) {
-                System.out.println("NO");;
+                System.out.println("NO");
+                return;
             }
         }
 
         System.out.println("YES");;
-
     }
 
     public void ex5() {
